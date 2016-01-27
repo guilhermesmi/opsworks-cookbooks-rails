3e290cb384c458c346db27c717e10b48034d4b8e
Antonio Grass <antonio_grass@hotmail.com>
2016-01-27 19:06:25 -0300

    change recipe awscli

--- awscli/recipes/deregister_classiclink.rb
+++ awscli/recipes/deregister_classiclink.rb
@@ -1,3 +1,5 @@
+include_recipe "awscli"
+
 execute "deregister" do
   command "/usr/bin/env AWS_ACCESS_KEY_ID=#{node['aws']['AWS_ACCESS_KEY_ID']} AWS_SECRET_ACCESS_KEY=#{node['aws']['AWS_SECRET_ACCESS_KEY']} aws ec2 detach-classic-link-vpc --instance-id #{node[:opsworks][:instance][:aws_instance_id]}  --vpc-id #{node[:aws][:classic_link][:vpc_id]} --region #{node[:opsworks][:instance][:region]}"
   user "ubuntu"
