fd0dd77643ffa8ecb9df7fb1041be1fb39756586
Antonio Grass <antonio_grass@hotmail.com>
2016-01-28 10:56:42 -0300

    add install aws cli

--- awscli/recipes/default.rb
+++ awscli/recipes/default.rb
@@ -1,6 +1,12 @@
 ## cookbooks/aws/recipes/default.rb
+execute "python-pip" do
+  command "sudo apt-get install -y python-pip"
+  user "root"
+  action :run
+end
+
 execute "aws-cli" do
-  command "sudo apt-get -y install awscli"
+  command "sudo pip install awscli"
   user "root"
   action :run
 end
