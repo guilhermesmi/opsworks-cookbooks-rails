ed71534fbe71a70ee2a3c8f29d8d8933858b9bb1
Guilherme Pires <guilherme.pires@ericsson.com>
2016-08-18 12:24:30 -0700

    Added mongodb cookbooks

---
+++ mongodb-cw/metatada.rb
@@ -0,0 +1,9 @@
+name        		'mongodb-cw'
+description 		'Example cookbook for MongoDB Berkshelf based install'
+maintainer 			'Guilherme Pires@ericsson.com'
+maintainer_email	'guilherme.pires@ericsson.com'
+license     		'Apache 2.0'
+version     		'1.0.0'
+
+depends "mongodb"
+
